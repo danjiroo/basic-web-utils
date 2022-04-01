519326d8c31647ae882a44d412a552bf5d2959ac
Eugene Peter Maestrado <eugenepeter@CEBMAC0002.local>
2022-04-01 17:41:49 +0800

    installed axios

--- src/useOIDC/machine/options/services.ts
+++ src/useOIDC/machine/options/services.ts
@@ -3,10 +3,10 @@
 /* eslint-disable @typescript-eslint/no-extra-semi */
 /* eslint-disable indent */
 
-import { Sender } from 'xstate'
+import { Sender } from 'xstate';
 
-import { Context, AuthorizedEvent } from '../types'
-import axios from 'axios'
+import { Context, AuthorizedEvent } from '../types';
+import axios from 'axios';
 import {
   AuthorizationServiceConfiguration,
   AuthorizationRequest,
@@ -18,10 +18,10 @@ import {
   AuthorizationNotifier,
   TokenRequest,
   GRANT_TYPE_AUTHORIZATION_CODE,
-} from '@openid/appauth'
-import { NoHashQueryStringUtils } from '../../noHashQueryStringUtils'
+} from '@openid/appauth';
+import { NoHashQueryStringUtils } from '../../noHashQueryStringUtils';
 
-import { convertToQueryParams } from '../../utils'
+import { convertToQueryParams } from '../../utils';
 
 const {
   // REACT_APP_LOGIN_URI = 'https://login.staging.pandolink.com',
@@ -31,20 +31,20 @@ const {
   REACT_APP_SCOPE = '',
   REACT_APP_CLIENT_SECRET = '',
   REACT_APP_CLIENT_ID = '',
-} = process.env
+} = process.env;
 
 const authorizationHandler = new RedirectRequestHandler(
   new LocalStorageBackend(),
   new NoHashQueryStringUtils(),
   window.location,
   new DefaultCrypto()
-)
+);
 export const services: any = {
   checkAuthorization:
     ({ accessToken }: Context) =>
     async (send: Sender<AuthorizedEvent>) => {
       if (!accessToken) {
-        ;(async () => {
+        (async () => {
           try {
             AuthorizationServiceConfiguration.fetchFromIssuer(
               REACT_APP_AUTH_SERVER!,
@@ -65,28 +65,28 @@ export const services: any = {
                   prompt: 'login',
                   access_type: 'offline',
                 },
-              })
+              });
 
               authorizationHandler.performAuthorizationRequest(
                 response,
                 authRequest
-              )
-              response && send('AUTHORIZED')
-            })
+              );
+              response && send('AUTHORIZED');
+            });
           } catch (error) {
-            console.log('ERROR AUTHORIZING:', error)
+            console.log('ERROR AUTHORIZING:', error);
           }
-        })()
+        })();
       }
     },
 
   checkAuthentication: () => (send: Sender<any>) => {
-    ;(() => {
-      const tokenHandler = new BaseTokenRequestHandler(new FetchRequestor())
-      const notifier = new AuthorizationNotifier()
-      authorizationHandler.setAuthorizationNotifier(notifier)
+    (() => {
+      const tokenHandler = new BaseTokenRequestHandler(new FetchRequestor());
+      const notifier = new AuthorizationNotifier();
+      authorizationHandler.setAuthorizationNotifier(notifier);
       notifier.setAuthorizationListener((request, response, error) => {
-        if (error) console.log('SET AUTHORIZATION LISTENER ERROR', error)
+        if (error) console.log('SET AUTHORIZATION LISTENER ERROR', error);
         //  CREATE TOKEN REQUEST
         const requestToken = new TokenRequest({
           client_id: REACT_APP_CLIENT_ID!,
@@ -102,7 +102,7 @@ export const services: any = {
                   scope: REACT_APP_SCOPE!,
                 }
               : {},
-        })
+        });
         AuthorizationServiceConfiguration.fetchFromIssuer(
           REACT_APP_AUTH_SERVER!,
           new FetchRequestor()
@@ -111,7 +111,7 @@ export const services: any = {
             tokenHandler.performTokenRequest(response, requestToken)
           )
           .then((response) => {
-            const { accessToken } = response
+            const { accessToken } = response;
             accessToken &&
               send({
                 type: 'AUTHENTICATED',
@@ -119,14 +119,14 @@ export const services: any = {
                   ...response,
                   isAuthenticated: true,
                 },
-              })
+              });
           })
           .catch((error) => {
-            send({ type: 'AUTHENTICATION_ERROR', payload: error })
-          })
-      })
-      authorizationHandler.completeAuthorizationRequestIfPossible()
-    })()
+            send({ type: 'AUTHENTICATION_ERROR', payload: error });
+          });
+      });
+      authorizationHandler.completeAuthorizationRequestIfPossible();
+    })();
   },
   logOutUser:
     ({ idToken }: Context) =>
@@ -134,16 +134,16 @@ export const services: any = {
       const queryParams = convertToQueryParams({
         id_token_hint: idToken,
         post_logout_redirect_uri: REACT_APP_REDIRECT_URI,
-      })
+      });
       const { data } = await axios.get(
         `${REACT_APP_AUTH_SERVER_LOGOUT}${queryParams}`
-      )
+      );
       // window.location.href = REACT_APP_AUTH_SERVER_LOGOUT + queryParams
       if (data) {
-        console.log('DATA DATA:', data)
+        console.log('DATA DATA:', data);
         send({
           type: 'LOG_OUT_SUCCESS',
-        })
+        });
       }
     },
-}
+};
